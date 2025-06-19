567a5b36206e848af9b3f5a33e056cb290269d09
PragnasriBendi <113855732+PragnasriBendi@users.noreply.github.com>
2025-06-19 06:55:04 +0530

    Add files via upload
    Basic code of extent Report-1(R)

---
+++ ExtentReport.java
@@ -0,0 +1,20 @@
+package postAssessment;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+public class ExtentReport {
+	
+	public static ExtentReports getReportObject() {
+		String path = System.getProperty("user.dir")+"//reports1//index.html";
+		ExtentSparkReporter reporter = new ExtentSparkReporter(path);
+		reporter.config().setReportName("Web Automation Results");
+		reporter.config().setDocumentTitle("Test Results");
+		
+		ExtentReports extent = new ExtentReports();
+		extent.attachReporter(reporter);
+		extent.setSystemInfo("Tester", "Pragnasri Bendi");
+		return extent;
+	}
+
+}
